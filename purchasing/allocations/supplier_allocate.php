0b21df79da76a0b7293b318cef737546f91bb887
notrinos <trananhphuong83@gmail.com>
2022-03-13 08:39:02 +0700

    translatable string typo

--- purchasing/allocations/supplier_allocate.php
+++ purchasing/allocations/supplier_allocate.php
@@ -52,7 +52,7 @@ function edit_allocations_for_transaction($type, $trans_no) {
 	display_heading2(_('Total:').' <b>'.price_format(-$cart->bank_amount).' '.$cart->currency.'</b>');
 
 	if (floatcmp($cart->bank_amount, $cart->amount)) {
-		$total = _('Amount ot be settled:').' <b>'.price_format(-$cart->amount).' '.$cart->person_curr.'</b>';
+		$total = _('Amount to be settled:').' <b>'.price_format(-$cart->amount).' '.$cart->person_curr.'</b>';
 		if ($cart->currency != $cart->person_curr)
 			$total .= sprintf(' (%s %s/%s)',  exrate_format($cart->bank_amount/$cart->amount), $cart->currency, $cart->person_curr);
 		display_heading2($total);
