cdbb775013f4e5889a97e07daa174fdfcff2e621
notrinos <trananhphuong83@gmail.com>
2026-08-07 23:15:39 +0700

    verify approval mail command arguments

--- admin/approval_cron.php
+++ admin/approval_cron.php
@@ -211,6 +211,7 @@ if ($run_emails || isset($_POST['RunBoth'])) {
         label_row(_('Transport Encoding Unavailable'), $email_summary['transport_encoding_unavailable']);
         label_row(_('Transport Argument Escape Unavailable'), $email_summary['transport_arg_escape_unavailable']);
         label_row(_('Transport Forced Params Override'), $email_summary['transport_forced_params_override']);
+        label_row(_('Transport Arguments Incompatible'), $email_summary['transport_args_incompatible']);
         label_row(_('Invalid Notification Rows'), $email_summary['invalid_notification']);
         label_row(_('State Transition Failed'), $email_summary['state_failed']);
         label_row(_('Claim Failed'), $email_summary['claim_failed']);
