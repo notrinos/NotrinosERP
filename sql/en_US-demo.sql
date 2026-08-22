e1237aa0908658acf0cb5cb6d472b2bfb44d8e15
notrinos <trananhphuong83@gmail.com>
2026-08-22 18:23:08 +0700

    add establishment foundation

--- sql/en_US-demo.sql
+++ sql/en_US-demo.sql
@@ -1405,6 +1405,24 @@ CREATE TABLE `0_hrm_jobs` (
 	CONSTRAINT `0_hrm_jobs_legal_entity_fk` FOREIGN KEY (`legal_entity_id`) REFERENCES `0_hrm_legal_entities` (`legal_entity_id`) ON UPDATE RESTRICT ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
+CREATE TABLE `0_hrm_establishments` (
+	`establishment_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`legal_entity_id` bigint(20) unsigned NOT NULL,
+	`establishment_code` varchar(32) NOT NULL,
+	`establishment_name` varchar(120) NOT NULL,
+	`establishment_status` varchar(16) NOT NULL DEFAULT 'active',
+	`reference_quality` varchar(32) NOT NULL DEFAULT 'greenfield_normalized_only',
+	`source` varchar(32) NOT NULL DEFAULT 'greenfield_normalized',
+	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`created_by` smallint(5) unsigned NOT NULL DEFAULT '0',
+	`updated_at` datetime DEFAULT NULL,
+	`updated_by` smallint(5) unsigned DEFAULT NULL,
+	PRIMARY KEY (`establishment_id`),
+	UNIQUE KEY `hrm_establishment_entity_code_uq` (`legal_entity_id`,`establishment_code`),
+	KEY `hrm_establishment_status_idx` (`legal_entity_id`,`establishment_status`,`establishment_id`),
+	CONSTRAINT `0_hrm_establishments_legal_entity_fk` FOREIGN KEY (`legal_entity_id`) REFERENCES `0_hrm_legal_entities` (`legal_entity_id`) ON UPDATE RESTRICT ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
 CREATE TABLE `0_hrm_positions` (
 	`position_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	`legal_entity_id` bigint(20) unsigned NOT NULL,
